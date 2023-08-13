a876870af3fc0ca92e29c7fa4426fca96fec881a
Aravindhan-a-p <11a02cnn@gmail.com>
2023-08-13 23:58:09 +0530

    commit

--- tor/script.sh
+++ tor/script.sh
@@ -1,18 +1,17 @@
 #! /usr/bin/bash
 sudo apt update
 sudo apt install -y tor
-hiddendir= /var/lib/tor/hidden_service/
-hiddenport= 80 127.0.0.1:80
+
 read -p "enter name of html page" name
 
-echo "HiddenServiceDir $hiddendir" | sudo tee -a /etc/tor/torrc
-echo  "HiddenServicePort $hiddenport" | sudo tee -a /etc/torr/torrc
+echo "HiddenServiceDir /var/lib/tor/hidden_service/" | sudo tee -a /etc/tor/torrc
+echo  "HiddenServicePort 80 127.0.0.1:80" | sudo tee -a /etc/tor/torrc
 sudo systemctl restart tor
 hostname=$(sudo cat /var/lib/tor/hidden_service/hostname)
 
 sudo apt install -y nginx
 
-sudo tee /etc/nginx/sites-available/"$hostname"+".conf" > /dev/null << EOF
+sudo tee /etc/nginx/sites-available/"$hostname.conf" > /dev/null << EOF
 server {
     listen 80;
     listen [::]:80;
